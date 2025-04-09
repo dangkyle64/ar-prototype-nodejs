a343d4f70e307e56491285f9ca0b3e49272d5630
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-09 09:46:13 -0700

    initial commit
    author: dangkyle64
    date: 4/9/2025

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+
+//route here
+import uploadVideoRoute from './routers/uploadVideoRoutes.js';
+
+const app = express();
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://ar-furniture-ecru.vercel.app',
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        console.log('Request Origin:', origin);
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));  
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+    allowedHeaders: ['Content-Type', 'Authorization'],  
+    credentials: true, 
+}));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const port = 5000;
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+};
