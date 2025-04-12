f2ea4f35e671518888fdd2dde54df17420413859
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-04-12 10:35:37 -0700

    update the imports for the controller
    author: dangkyle64
    date: 4/12/2025

--- routers/uploadVideoRouter.js
+++ routers/uploadVideoRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { uploadVideoController } from '../controllers/uploadVideoController';
+import { uploadVideoController } from '../controllers/uploadVideoController.js';
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
